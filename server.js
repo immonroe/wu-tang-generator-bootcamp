2bc5375716105e75f1da12d943bb19a407720044
immonroe <immanuelmonroe@gmail.com>
2025-04-03 15:02:35 -0400

    init commit

---
+++ server.js
@@ -0,0 +1,83 @@
+const http = require('http');
+const fs = require('fs')
+const url = require('url');
+const querystring = require('querystring');
+const figlet = require('figlet')
+
+const server = http.createServer(function(req, res) {
+  const page = url.parse(req.url).pathname;
+  const params = querystring.parse(url.parse(req.url).query);
+  console.log(page);
+  if (page == '/') {
+    fs.readFile('index.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+      
+    });
+  }
+  else if (page == '/otherpage') {
+    fs.readFile('otherpage.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/otherotherpage') {
+    fs.readFile('otherotherpage.html', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (page == '/api') {
+    let choice = Math.floor(Math.random() * 2)
+    console.log(choice)
+    if('student' in params){
+      console.log(params['student'])
+      if (params['student'] === params['student'].toLowerCase().split("").reverse().join("")) {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          name: `Your word is ${params['student']}`,
+          status: "This is a palindrome!",
+          currentOccupation: "test"
+        }
+        res.end(JSON.stringify(objToJson));
+      }//student = leon
+      
+      else if (params['student'] != 'leon'){
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        const objToJson = {
+          name: `Your word is ${params['student']}`,
+          status: "This is not a palindrome...",
+          currentOccupation: "test two"
+        }
+        res.end(JSON.stringify(objToJson));
+      }//student != leon
+    }//student if
+  }//else if
+  else if (page == '/css/style.css'){
+    fs.readFile('css/style.css', function(err, data) {
+      res.write(data);
+      res.end();
+    });
+  }else if (page == '/js/main.js'){
+    fs.readFile('js/main.js', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data);
+      res.end();
+    });
+  }else{
+    figlet('404!!', function(err, data) {
+      if (err) {
+          console.log('Something went wrong...');
+          console.dir(err);
+          return;
+      }
+      res.write(data);
+      res.end();
+    });
+  }
+});
+
+server.listen(8000);
