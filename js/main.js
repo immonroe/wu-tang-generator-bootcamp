16772be5fbeaee8b73bff31ddd6a4ccad609a911
immonroe <immanuelmonroe@gmail.com>
2025-04-04 19:17:09 -0400

    Added function to backend and donald glover easter egg

--- js/main.js
+++ js/main.js
@@ -1,48 +1,17 @@
-document.querySelector('#clickMe').addEventListener('click', makeReq)
+document.querySelector('#clickMe').addEventListener('click',  () => {
 
-function makeReq(){
+  const firstName = document.getElementById("firstName").value.toLowerCase()
+  const lastName = document.getElementById("lastName").value.toLowerCase()
+  const faveAnimal = document.getElementById("faveAnimal").value.toLowerCase()
+  const faveSeason = document.getElementById("faveSeason").value.toLowerCase()
+  const faveColor = document.getElementById("faveColor").value.toLowerCase()
 
-  const userName = document.querySelector("#userName").value;
+  const url = `/api?firstName=${firstName}&lastName=${lastName}&faveAnimal=${faveAnimal}&faveSeason=${faveSeason}&faveColor=${faveColor}`;
 
-  fetch(`/api?student=${userName}`)
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data);
-      document.querySelector("#personName").textContent = data.name
-      document.querySelector("#personStatus").textContent = data.status
-      // document.querySelector("#personOccupation").textContent = data.currentOccupation
-    });
-
-}
-
-// document.getElementById("clickMe").onclick = makeReq;
-//
-// function makeReq(){
-//
-//   var userName = document.getElementById("userName").value;
-//
-//   var request = new XMLHttpRequest();
-//   request.open('GET', '/api?student='+userName, true);
-//
-//   request.onload = function() {
-//       console.log("works")
-//       if (request.status >= 200 && request.status < 400) {
-//         // Success!
-//         var data = JSON.parse(request.responseText);
-//         console.log(data)
-//         document.getElementById("personName").innerHTML = data.name
-//         document.getElementById("personStatus").innerHTML = data.status
-//         document.getElementById("personOccupation").innerHTML = data.currentOccupation
-//
-//       } else {
-//         // We reached our target server, but it returned an error
-//
-//       }
-//     };
-//
-//     request.onerror = function() {
-//       // There was a connection error of some sort
-//     };
-//
-//     request.send();
-// }
+  fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      document.querySelector("#result").innerHTML = data.name;
+    })
+    .catch(err => console.error("API error:", err));
+});
--- server.js
+++ server.js
@@ -31,30 +31,63 @@ const server = http.createServer(function(req, res) {
     });
   }
   else if (page == '/api') {
-    let choice = Math.floor(Math.random() * 2)
-    console.log(choice)
-    if('student' in params){
-      console.log(params['student'])
-      if (params['student'] === params['student'].toLowerCase().split("").reverse().join("")) {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: `Your word is ${params['student']}`,
-          status: "This is a palindrome!",
-          currentOccupation: "test"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student = leon
+    const { firstName, lastName, faveAnimal, faveSeason, faveColor } = params
+
+    const nameParts1 = ["RZA", "Mystic", "Ol' Dirty", "Blunted", "Iron", "Golden", "Silent", "Ghost"]
+    const nameParts2 = ["Monk", "Samurai", "Warlord", "Disciple", "Shogun", "Poet", "Bandit", "Phantom"]
+    const animalParts = ["of the Wolf", "the Panther Style", "Crane Killa", "Tiger Fist", "Bear Claw", "Dragon Breath"]
+    const seasonParts = ["in the Spring", "of Autumn Flame", "of the Winter Wind", "during Summer Madness"]
+    const colorParts = ["Wearing Crimson", "Dressed in Gold", "Cloaked in Shadow", "Wielding Silver", "Bleeding Black"]
+  
+    // function to find the index of an array comparing array position to string.length
+    // added trim to account for edge cases where users include white space in answer
+    function getIndex(str, arr) {
+      return str?.trim().length % arr.length
+    }
+  
+    // CG easter egg
+    function generateWuName(first, last, animal, season, color) {
+      if (first?.toLowerCase() === "donald" && last?.toLowerCase() === "glover") {
+        return "Childish Gambino"
+      }
       
-      else if (params['student'] != 'leon'){
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        const objToJson = {
-          name: `Your word is ${params['student']}`,
-          status: "This is not a palindrome...",
-          currentOccupation: "test two"
-        }
-        res.end(JSON.stringify(objToJson));
-      }//student != leon
-    }//student if
+      // Plug in arrays to get indexes for each part of the values inputted - getIndex() also can idenify if value is not found
+      const part1 = nameParts1[getIndex(first, nameParts1)]
+      const part2 = nameParts2[getIndex(last, nameParts2)]
+      const part3 = animalParts[getIndex(animal, animalParts)]
+      const part4 = seasonParts[getIndex(season, seasonParts)]
+      const part5 = colorParts[getIndex(color, colorParts)]
+  
+      return `${part1} ${part2} ${part3} ${part4} ${part5}`
+    }
+  
+    const wuName = generateWuName(firstName, lastName, faveAnimal, faveSeason, faveColor)
+  
+    res.writeHead(200, {'Content-Type': 'application/json'})
+    res.end(JSON.stringify({name:wuName}))
+
+    // if('cardOne' in params){
+    //   console.log(params['cardOne'])
+    //   if (params['cardOne'] === params['cardTwo']) {
+    //     res.writeHead(200, {'Content-Type': 'application/json'});
+    //     const objToJson = {
+    //       name: `You selected: ${params['cardOne']} and ${params['cardTwo']}`,
+    //       status: "This is a palindrome!",
+    //       currentOccupation: "test"
+    //     }
+    //     res.end(JSON.stringify(objToJson));
+    //   }//student = leon
+      
+    //   else if (params['cardOne'] != params['cardTwo']){
+    //     res.writeHead(200, {'Content-Type': 'application/json'});
+    //     const objToJson = {
+    //       name: `You selected: ${params['cardOne']} and ${params['cardTwo']}`,
+    //       status: "This is not a palindrome...",
+    //       currentOccupation: "test two"
+    //     }
+    //     res.end(JSON.stringify(objToJson));
+    //   }//student != leon
+    // }//student if
   }//else if
   else if (page == '/css/style.css'){
     fs.readFile('css/style.css', function(err, data) {
